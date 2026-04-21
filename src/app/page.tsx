a67b63decad4fb276cb11f1d0c6d2bebe7145a45
idsp0731-gif <idsp.0731@gmail.com>
2026-04-21 23:15:52 +0000

    fix: 投資セクションの順番変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,8 +44,8 @@ export default async function Home() {
       <StickyNav />
       <About />
       <CmsArticles initialArticles={articles} />
-      <InvestmentService />
       <RecommendedProperties initialProperties={investments} />
+      <InvestmentService />
       <SalesResidentialService />
       <RentalService initialRentals={rentals} />
       <LineCtaBanner1 />
